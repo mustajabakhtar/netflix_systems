e7c0ae4e7853214f59572d15f4bf3a5d3478ff65
Mustajab <Mustajab.Akhtar@makemytrip.com>
2017-03-08 22:31:46 +0530

    Initial commit

---
+++ src/main/java/com/mustajab/netflix/system/main/NetflixSystemMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mustajab.netflix.system.main;
+
+/**
+ * @author Mustajab Akhtar
+ * @since 1.0
+ */
+public class NetflixSystemMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
